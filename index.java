ebeedbfa09342700a2fc5460de09e2a738bef9c9
660853 <jj2497@comcast.net>
2020-09-15 16:12:24 +0000

    Made my scary creature.

--- index.java
+++ index.java
@@ -11,6 +11,31 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
+fill(255);
+ellipse(160,110,220,220);
+fill(255,0,0);
+ellipse(125,60,25,25);
+ellipse(200,60,25,25);
+fill(0);
+rect(120,25,40,10);
+rect(195,25,40,10);
+fill(139,75,75);
+rect(125,145,10,40);
+rect(165,130,85,10);
+rect(205,145,10,40);
+fill(175,30,30);
+ellipse(125,60,8,8);
+ellipse(200,60,8,8);
+fill(215,145,25);
+ellipse(80,100,10,10);
+ellipse(175,50,10,10);
+ellipse(180,190,10,10);
+ellipse(80,60,10,10);
+ellipse(220,130,10,10);
+
+
+
+
   
  
   
